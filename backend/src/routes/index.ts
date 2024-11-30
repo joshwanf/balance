2ddbbe3761f6b0148318aae8ac9d97b35519168a
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-11-30 14:12:32 -0500

    fixed production build paths to serve static site

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -10,19 +10,23 @@ if (process.env.NODE_ENV === "production") {
   const path = require("path")
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
+    console.log(
+      path.resolve(__dirname, "../../../../frontend", "dist", "index.html")
+    )
     // res.cookie("XSRF-TOKEN", req.csrfToken())
     res.sendFile(
-      path.resolve(__dirname, "../../../frontend", "dist", "index.html")
+      path.resolve(__dirname, "../../../../frontend", "dist", "index.html")
     )
   })
   // Serve the static assets in the frontend's build folder
+  console.log("static assets", path.resolve("../frontend/dist"))
   router.use(express.static(path.resolve("../frontend/dist")))
 
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     // res.cookie("XSRF-TOKEN", req.csrfToken())
     res.sendFile(
-      path.resolve(__dirname, "../../../frontend", "dist", "index.html")
+      path.resolve(__dirname, "../../../../frontend", "dist", "index.html")
     )
   })
 }
