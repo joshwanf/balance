5b4102c944280b273231e898e05cef363354e7be
joshwanf <17016446+joshwanf@users.noreply.github.com>
2024-12-06 00:56:35 -0500

    added feature to landing page

--- frontend/src/components/Landing/Landing.tsx
+++ frontend/src/components/Landing/Landing.tsx
@@ -6,6 +6,7 @@ import { AnimatePresence } from "motion/react"
 import { Modal } from "../../lib/ComponentLibrary/Modal"
 import { SignupForm } from "./SignupForm"
 import { useNavigate } from "react-router"
+import { Search } from "lucide-react"
 
 interface Props {}
 export const Landing: React.FC<Props> = props => {
@@ -53,7 +54,7 @@ export const Landing: React.FC<Props> = props => {
       {/* <div className="flex flex-row justify-around bg-dark-beige rounded-b-3xl pb-4 px-6 text-slate-500"> */}
       <h1 className="text-4xl py-4 pl-4 bg-dark-beige text-slate-600">
         Why should you{" "}
-        <span className="text-grass-500 bg-slate-700 px-4 py-2 rounded-lg">
+        <span className="text-grass-500 bg-slate-700 px-4 py-2 mx-2 rounded-lg inline-block -skew-x-12">
           Balance
         </span>
         ?
@@ -62,7 +63,7 @@ export const Landing: React.FC<Props> = props => {
         <div className="py-10 w-1/3 text-2xl">
           <div className="text-xl">
             <p className="py-4">Custom Money class* with infix notation!</p>
-            <ul className="py-4 list-disc list-inside">
+            <ul className="py-4 list-disc list-inside leading-normal">
               <li>
                 Safely pass strings from user input with{" "}
                 <code className="bg-grass-500 px-2 py-0.5 rounded-lg text-grass-800">
@@ -94,18 +95,34 @@ export const Landing: React.FC<Props> = props => {
         </div>
       </div>
       <div className="flex flex-row justify-around px-10 text-slate-600">
-        <div className="self-center">
-          <Btn.SecondaryButton
-            onClick={startBalancing}
-            className="inline-block px-6 py-2 bg-slate-500 text-white rounded-lg hover:bg-slate-900 transition-all duration-1000"
-          >
-            Start Balancing
-          </Btn.SecondaryButton>
+        <div className="p-10 w-1/2 text-2xl border-grass-300 bg-grass-100 rounded-xl drop-shadow-xl">
+          <div className="text-xl text-slate-600">
+            <div className="flex border-2 border-grass-600 rounded-lg p-2">
+              <Search className="text-grass-600" />
+              <input
+                readOnly={true}
+                className="bg-transparent text-base w-full focus:outline-none placeholder:text-grass-600"
+                placeholder='endDate:2024-12-06 tags:"capstone, coffee"'
+              />
+            </div>
+            <p className="py-4">Search! (lite)</p>
+            <p className="py-4">
+              A very primitive query language to quickly find transactions
+            </p>
+          </div>
         </div>
-        <div className="p-10 w-1/3 text-2xl border-grass-300 border-4 bg-grass-50 rounded-xl">
+        <div className="p-10 w-1/3 text-2xl border-grass-300 bg-grass-100 rounded-xl drop-shadow-xl">
           <div className="text-xl text-slate-600">
             <p className="py-4">This app is written in TypeScript!</p>
             <p className="py-4">for a much better... typing... experience...</p>
+          </div>
+          <div className="self-center">
+            <Btn.SecondaryButton
+              onClick={startBalancing}
+              className="inline-block px-6 py-2 bg-slate-500 text-white rounded-lg hover:bg-slate-900 transition-all duration-1000 shadow-xl"
+            >
+              Start Balancing
+            </Btn.SecondaryButton>
           </div>
         </div>
       </div>
